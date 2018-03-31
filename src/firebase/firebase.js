9ef6c065871009f4a209ff8d9a7396af8f971929
sivarasan ponnuthurai <rasanthurai1992@gmail.com>
2018-03-31 18:30:25 +0530

    firebase changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: AIzaSyAapA5P8gWC83aHK26xgDDMzIBC4dw914w,
+  authDomain: expensify-a13e6.firebaseapp.com,
+  databaseURL: https://expensify-a13e6.firebaseio.com,
+  projectId: expensify-a13e6,
+  storageBucket: expensify-a13e6.appspot.com,
+  messagingSenderId: 124728096317
+
 };
 console.log('config', config )
 firebase.initializeApp(config);
